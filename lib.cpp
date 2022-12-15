d01f64aee623c90cfb2cb266fd5d9a1363218678
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-12-15 22:10:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
 
+bool comp(char elenco[10][20], int &p, char nome[]){
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++){
+            if(elenco[i][j] != nome[j]){
+                j = 20;
+            }else{
+                if(j==19){
+                    p = i;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
